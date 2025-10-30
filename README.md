ee48e1fb4c5b982990041a4f6db7ad6df27c5129
Mathieu Robert <mathieu1.robert@epitech.eu>
2025-10-30 22:16:20 +0000

    feat(Root): Change React by Java

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # My-Scrabble-Client
-React Client for MyScrabble game
+Java Client for MyScrabble game
+
+## How to run ?
+The following documentation will help you to run the client using Maven or Docker.
+
+### Run using Maven
+If you have Maven installed on your computer, you can compile the project at the root of the repository using:
+```
+mvn compile
+```
+
+Then run the project using:
+```
+mvn exec:java
+```
+
+### Run using Docker
+If you have and prefer to use Docker to run the project, run the following command on your terminal at the project root:
+```
+docker-compose up --build
+```
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import java.net.URI;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private WebSocketClient client;
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Scrabble Client");
+
+        BorderPane root = new BorderPane();
+        Button connectButton = new Button("Connect to Server");
+
+        connectButton.setOnAction(e -> connectToServer());
+        root.setCenter(connectButton);
+
+        Scene scene = new Scene(root, 400, 300);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void connectToServer() {
+        try {
+            String url = "wss://my-scrabble-backend.onrender.com";
+
+            client = new WebSocketClient(new URI(url)) {
+                @Override
+                public void onOpen(ServerHandshake handshake) {
+                    System.out.println("Connected to Scrabble server!");
+                }
+
+                @Override
+                public void onMessage(String message) {
+                    System.out.println("Message: " + message);
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    System.out.println("Connection closed: " + reason);
+                }
+
+                @Override
+                public void onError(Exception ex) {
+                    ex.printStackTrace();
+                }
+            };
+            client.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
