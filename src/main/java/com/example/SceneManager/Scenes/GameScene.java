e3dbb96e9748952db1d267497d19520cfb8396c5
Mathieu Robert <mathieu1.robert@epitech.eu>
2025-11-14 15:08:10 +0000

    feat(GameScene): Create code architecture for game scene class

--- src/main/java/com/example/SceneManager/Scenes/GameScene.java
+++ src/main/java/com/example/SceneManager/Scenes/GameScene.java
@@ -2,10 +2,36 @@ package com.example.SceneManager.Scenes;
 
 import com.example.SceneManager.MyScene;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+
 public class GameScene extends MyScene {
 
     @Override
     public void initRoot() {
+
+         // Root container
+        _root.setPadding(new Insets(20));
+        _root.setBackground(new Background(new BackgroundFill(Color.web("0x16161E"), null, null)));
+        _root.setAlignment(Pos.CENTER);
+
+        header();
+        boardView();
+        rightSidePanel();
+    }
+
+    private void header() {
+
+    }
+
+    private void boardView() {
+
+    }
+
+    private void rightSidePanel() {
 
     }
 }
